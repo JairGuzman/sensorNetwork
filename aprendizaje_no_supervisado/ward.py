b28836f0c20297912e0e78b87482508f343d3aae
Jair <jair43253@gmail.com>
2024-11-07 15:20:38 -0800

    fdsfd

--- aprendizaje_no_supervisado/ward.py
+++ aprendizaje_no_supervisado/ward.py
@@ -5,7 +5,7 @@ import scipy.cluster.hierarchy as sch # Contiene funciones para realizar el clus
 from scipy.cluster.hierarchy import dendrogram, linkage, fcluster
 
 minute = 1;
-while minute < 12:
+while minute < 14:
   url = 'https://raw.githubusercontent.com/JairGuzman/datasets/main/minute'+ str(minute) + '.csv' # URL del archivo CSV
   estudiantes=pd.read_csv(url, engine='python', index_col=0) #Leemos el archivo CSV
   lecturas = estudiantes.iloc[:, [0,1,2,3,4,5,6,7,8,9,10,11]].values #Seleccionamos las columnas que contienen las lecturas
